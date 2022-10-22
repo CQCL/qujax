9bf3b8b3576930e96cf9ac1bebd8e05565a817d5
Gabriel Matos <pygdfm@leeds.ac.uk>
2022-10-22 22:23:30 +0100

    Add test_params_to_densitytensor_func

--- tests/test_density_matrix.py
+++ tests/test_density_matrix.py
@@ -1,7 +1,8 @@
 from jax import numpy as jnp, jit
 
 import qujax
-from qujax.density_matrix import _kraus_single
+from qujax.density_matrix import _kraus_single, get_params_to_densitytensor_func
+from qujax import get_params_to_statetensor_func
 
 
 def test_kraus_single():
@@ -26,3 +27,26 @@ def test_kraus_single():
     qujax_kraus_dm_jit = qujax_kraus_dt_jit.reshape(dim, dim)
     assert jnp.all(jnp.abs(qujax_kraus_dm_jit - check_kraus_dm) < 1e-5)
 
+
+def test_params_to_densitytensor_func():
+    n_qubits = 2
+
+    gate_seq = ["Rx" for _ in range(n_qubits)]
+    qubit_inds_seq = [(i,) for i in range(n_qubits)]
+    param_inds_seq = [(i,) for i in range(n_qubits)]
+
+    gate_seq += ["CZ" for _ in range(n_qubits - 1)]
+    qubit_inds_seq += [(i, i+1) for i in range(n_qubits - 1)]
+    param_inds_seq += [() for _ in range(n_qubits - 1)]
+
+    params_to_dt = get_params_to_densitytensor_func(gate_seq, qubit_inds_seq, param_inds_seq, n_qubits)
+    params_to_st = get_params_to_statetensor_func(gate_seq, qubit_inds_seq, param_inds_seq, n_qubits)
+
+    params = jnp.arange(n_qubits)/10.
+
+    st = params_to_st(params)
+    dt_test = (st.reshape(-1, 1) @ st.reshape(1, -1).conj()).reshape(2 for _ in range(2*n_qubits))
+
+    dt = params_to_dt(params)
+
+    assert jnp.allclose(dt, dt_test)
